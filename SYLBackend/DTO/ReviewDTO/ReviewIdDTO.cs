f8e46856b15a7952068f868f0c9d660589cc0810
ddovile <71456312+ddovile@users.noreply.github.com>
2021-04-18 03:28:54 +0300

    mini pakeitimas

--- SYLBackend/DTO/ReviewDTO/ReviewIdDTO.cs
+++ SYLBackend/DTO/ReviewDTO/ReviewIdDTO.cs
@@ -8,5 +8,9 @@ namespace SYLBackend.DTO.ReviewDTO
     public class ReviewIdDTO
     {
         public string reviewId { get; set; }
+        public string customerId { get; set; }
+        public string shopId { get; set; }
+        public int reviewRating { get; set; }
+        public string reviewComment { get; set; }
     }
 }
